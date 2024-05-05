0374a531da8e68f3ebc17477fb878654d41bce2e
Cody Owens <owen1003@gmail.com>
2024-05-04 19:18:42 -0500

    Add about me info and fix styling

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule
+  ],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
